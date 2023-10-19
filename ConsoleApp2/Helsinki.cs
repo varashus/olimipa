3fe3879bc2f0e5b78197f2f6644f8975b6854657
varashus <rigodavid100@gmail.com>
2023-10-19 10:26:06 +0200

    Add project files.

---
+++ ConsoleApp2/Helsinki.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Helsinki
+    {
+        public Helsinki(string r)
+        {
+            var v = r.Split(' ');
+            this.helyezes = int.Parse(v[0]);
+            this.sportolokszama = int.Parse(v[1]);
+            this.sportag = v[2];
+            this.versenyszam = v[3];
+        }
+
+        public int helyezes { get; set; }
+        public int sportolokszama { get; set; }
+        public string sportag { get; set; }
+        public string versenyszam { get; set; }
+
+
+
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var sportolok = new List<Helsinki>();
+            using StreamReader sr = new StreamReader(
+                path: @"..\..\..\src\helsinki.txt",
+                encoding: System.Text.Encoding.UTF8
+                );
+            while (!sr.EndOfStream)
+            {
+                sportolok.Add(new Helsinki(sr.ReadLine()));   
+            }
+            Console.WriteLine(sportolok.Count());
+            
+            int arany = 0;
+            int ezust = 0;
+            int bronz = 0;
+            foreach (var x in sportolok)
+            {
+                if (x.helyezes == 1)
+                {
+                    arany++;    
+                }
+                if (x.helyezes == 2)
+                {
+                    ezust++;
+                }
+                if (x.helyezes == 3)
+                {
+                    bronz++;
+                }
+            }
+            Console.WriteLine($"arany: {arany}");
+            Console.WriteLine($"ezüst: {ezust}");
+            Console.WriteLine($"bronz: {bronz}");
+            Console.WriteLine($"összesen: {arany+ezust+bronz}");
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
